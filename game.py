f7e2f6c50d685c85e2347d59c64ec95fb62137c5
Asylzhan <75136099+Asylzhann@users.noreply.github.com>
2024-04-17 20:05:30 +0500

    Animation of movement to left side and removed unused parts of code

--- game.py
+++ game.py
@@ -13,20 +13,20 @@ text_surface = myfont.render('some text', True, 'Red')
 background = pygame.image.load('images/background.png')
 background = pygame.transform.scale(background, (800, 600))
 
-# player = pygame.image.load('images/player.png')
-# walk_left = [
-#     pygame.image.load('images/player_left/player_left1.png'),
-#     pygame.image.load('images/player_left/player_left2.png'),
-#     pygame.image.load('images/player_left/player_left3.png')
-# ]
+walk_left = [
+    pygame.image.load('images/player_left/player_left1.png'),
+    pygame.image.load('images/player_left/player_left2.png'),
+    pygame.image.load('images/player_left/player_left3.png')
+]
 walk_right = [
     pygame.image.load('images/player_right/player_right1.png'),
     pygame.image.load('images/player_right/player_right2.png'),
     pygame.image.load('images/player_right/player_right3.png')
 ]
 player_anim_count = 0
-is_moving = False
-
+is_moving_left = False
+is_moving_right = False
+last_movement = True
 
 class Player:
 
@@ -53,7 +53,6 @@ class Player:
             text_surface = myfont.render(stat, True, (255, 255, 255))
             screen.blit(text_surface, (10, i * 20))
 
-
 player = Player()
 
 running = True
@@ -64,35 +63,36 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and player.pos_x > 0:
         player.pos_x -= player.speed
-        is_moving = True
+        is_moving_left = True
+        last_movement = True
     elif keys[pygame.K_RIGHT] and player.pos_x < 760:
         player.pos_x += player.speed
-        is_moving = True
+        is_moving_right = True
+        last_movement = False
     else:
-        is_moving = False
+        is_moving_left = False
+        is_moving_right = False
 
-    if is_moving:
+    if is_moving_right:
         screen.blit(walk_right[player_anim_count],
                     (player.pos_x, player.pos_y))
         player_anim_count = (player_anim_count + 1) % len(walk_right)
+    elif is_moving_left:
+        screen.blit(walk_left[player_anim_count],
+                    (player.pos_x, player.pos_y))
+        player_anim_count = (player_anim_count + 1) % len(walk_left)
+    elif last_movement:
+        screen.blit(walk_left[0], (player.pos_x, player.pos_y))
     else:
         screen.blit(walk_right[0], (player.pos_x, player.pos_y))
-
-    if player_anim_count == 2:
-        player_anim_count = 0
-    else:
-        player_anim_count += 1
-    # screen.blit(text_surface, (100, 50))
-    # screen.blit(player, (200, 100))
 
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                screen.fill((70, 44, 133))
+            running = False
+        # elif event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_a:
 
     clock.tick(15)
