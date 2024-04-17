c97f5bf469bd3489efb12506364e03ca7ceaec8b
Asylzhan <75136099+Asylzhann@users.noreply.github.com>
2024-04-17 22:23:14 +0500

    Added start screen

--- game.py
+++ game.py
@@ -1,18 +1,26 @@
-import pygame, time
+import pygame
 
 clock = pygame.time.Clock()
 pygame.init()
-SIZE = WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode(SIZE)
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("proj n11")
+
+transparent_surface = pygame.Surface((WIDTH*0.8, HEIGHT*0.8), pygame.SRCALPHA)
+transparent_surface.fill((200, 200, 200, 200))
+greeting = True
+
 # icon = pygame.image.load('images/icon.png')
 # pygame.display.set_icon(icon)
 
 myfont = pygame.font.SysFont('comicsansms', 20)
-text_surface = myfont.render('some text', True, 'Red')
+
+button_surf = myfont.render('Start', True, 'white')
+button = pygame.Rect(200, 200, 110, 60)
+button.center = (WIDTH//2, HEIGHT//2 + 100)
 
 background = pygame.image.load('images/background.png')
-background = pygame.transform.scale(background, SIZE)
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
 walk_left = [
     pygame.image.load('images/player_left/player_left1.png'),
@@ -56,79 +64,68 @@ class Player:
 
 player = Player()
 
-
 running = True
-# Start game
-start = True 
-start_font1 = pygame.font.SysFont('arial', 35)
-start_hi = start_font1.render('Hi!', True, (255, 255, 255))
-start_hi_rect = start_hi.get_rect()
-start_hi_rect.center = (WIDTH//2, HEIGHT//2)
-
-start_font2 = pygame.font.SysFont('arial', 20)
-start_text1 = start_font2.render('To play this game imagine that you are a student of KBTU from another city.', True, (255, 255, 255))
-start_text2 = start_font2.render('Try to survive until the end of semester.', True, (255, 255, 255))
-start_text3 = start_font2.render('Remember, every action has its own consequences.', True, (255, 255, 255))
-
-start_text1_rect = start_text1.get_rect()
-start_text1_rect.center = (WIDTH//2, HEIGHT//2-20)
-start_text2_rect = start_text2.get_rect()
-start_text2_rect.center = (WIDTH//2, HEIGHT//2)
-start_text3_rect = start_text3.get_rect()
-start_text3_rect.center = (WIDTH//2, HEIGHT//2+20)
-
 while running:
-    
-    while start:
-        screen.fill((0, 0, 0))
-        screen.blit(start_hi, start_hi_rect)
-        pygame.display.update()
-        time.sleep(1)
-        screen.fill((0, 0, 0))
-        screen.blit(start_text1, start_text1_rect)
-        screen.blit(start_text2, start_text2_rect)
-        screen.blit(start_text3, start_text3_rect)
-        
-        pygame.display.update()
-        time.sleep(2)
-        start = False
-    
-    screen.blit(background, (0, 0))
-    player.draw_stats(screen)
-    
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player.pos_x > 0:
-        player.pos_x -= player.speed
-        is_moving_left = True
-        last_movement = True
-    elif keys[pygame.K_RIGHT] and player.pos_x < 760:
-        player.pos_x += player.speed
-        is_moving_right = True
-        last_movement = False
-    else:
-        is_moving_left = False
-        is_moving_right = False
-
-    if is_moving_right:
-        screen.blit(walk_right[player_anim_count],
-                    (player.pos_x, player.pos_y))
-        player_anim_count = (player_anim_count + 1) % len(walk_right)
-    elif is_moving_left:
-        screen.blit(walk_left[player_anim_count],
-                    (player.pos_x, player.pos_y))
-        player_anim_count = (player_anim_count + 1) % len(walk_left)
-    elif last_movement:
-        screen.blit(walk_left[0], (player.pos_x, player.pos_y))
-    else:
-        screen.blit(walk_right[0], (player.pos_x, player.pos_y))
-
-    pygame.display.update()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             running = False
-        # elif event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_a:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button.collidepoint(event.pos):
+                t = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                t = False
 
+    screen.blit(background, (0, 0))
+
+    if not greeting:
+        player.draw_stats(screen)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.pos_x > 0:
+            player.pos_x -= player.speed
+            is_moving_left = True
+            last_movement = True
+        elif keys[pygame.K_RIGHT] and player.pos_x < 760:
+            player.pos_x += player.speed
+            is_moving_right = True
+            last_movement = False
+        else:
+            is_moving_left = False
+            is_moving_right = False
+
+        if is_moving_right:
+            screen.blit(walk_right[player_anim_count], (player.pos_x, player.pos_y))
+            player_anim_count = (player_anim_count + 1) % len(walk_right)
+        elif is_moving_left:
+            screen.blit(walk_left[player_anim_count], (player.pos_x, player.pos_y))
+            player_anim_count = (player_anim_count + 1) % len(walk_left)
+        elif last_movement:
+            screen.blit(walk_left[0], (player.pos_x, player.pos_y))
+        else:
+            screen.blit(walk_right[0], (player.pos_x, player.pos_y))
+
+    else:
+        screen.blit(transparent_surface, (WIDTH*0.1, HEIGHT*0.1))
+        greeting_text = [
+            "Hi!",
+            "To play this game imagine that you are a student",
+            "of KBTU from another city.",
+            "Try to survive until the end of semester.",
+            "",
+            "Remember, every action has its own consequences."
+        ]
+        for i, text in enumerate(greeting_text):
+            text_surface = myfont.render(text, True, (0, 0, 0))
+            screen.blit(text_surface, (100, i * 30 + 100))
+
+        a, b = pygame.mouse.get_pos()
+        if button.x <= a <= button.x + 110 and button.y <= b <= button.y + 60:
+            pygame.draw.rect(screen, (180, 180, 180), button)
+        else:
+            pygame.draw.rect(screen, (110, 110, 110), button)
+        screen.blit(button_surf, (button.x + 26, button.y + 14))
+
+    pygame.display.update()
     clock.tick(15)
+pygame.quit()
