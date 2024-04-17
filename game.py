6d012741e69c3818b625f34cf1afd218a19f8954
Aisulu <aidarkyzyaisulu@gmail.com>
2024-04-17 19:05:09 +0500

    Create Player Class and stats

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ pygame.display.set_caption("proj n11")
 # icon = pygame.image.load('images/icon.png')
 # pygame.display.set_icon(icon)
 
-myfont = pygame.font.SysFont('comicsansms', 40)
+myfont = pygame.font.SysFont('comicsansms', 20)
 text_surface = myfont.render('some text', True, 'Red')
 
 background = pygame.image.load('images/background.png')
@@ -25,30 +25,58 @@ walk_right = [
     pygame.image.load('images/player_right/player_right3.png')
 ]
 player_anim_count = 0
-player_x = 300
-player_y = 420
-player_speed = 10
 is_moving = False
+
+
+class Player:
+
+    def __init__(self):
+        self.health = 70
+        self.grades = 100
+        self.happiness = 80
+        self.finances = 100000
+        self.sleepiness = 10
+        self.stress = 10
+        self.friends = 0
+        self.pos_x = 300
+        self.pos_y = 420
+        self.speed = 10
+
+    def draw_stats(self, screen):
+        stats = [
+            f"Health: {self.health}", f"Grades: {self.grades}",
+            f"Happiness: {self.happiness}", f"Finances:{self.finances} ₸",
+            f"Sleepiness: {self.sleepiness}", f"Stress: {self.stress}",
+            f"Friends: {self.friends}"
+        ]
+        for i, stat in enumerate(stats):
+            text_surface = myfont.render(stat, True, (255, 255, 255))
+            screen.blit(text_surface, (10, i * 20))
+
+
+player = Player()
 
 running = True
 while running:
     screen.blit(background, (0, 0))
+    player.draw_stats(screen)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_x > 0:
-        player_x -= player_speed
+    if keys[pygame.K_LEFT] and player.pos_x > 0:
+        player.pos_x -= player.speed
         is_moving = True
-    elif keys[pygame.K_RIGHT] and player_x < 760:
-        player_x += player_speed
+    elif keys[pygame.K_RIGHT] and player.pos_x < 760:
+        player.pos_x += player.speed
         is_moving = True
     else:
         is_moving = False
 
     if is_moving:
-        screen.blit(walk_right[player_anim_count], (player_x, player_y))
+        screen.blit(walk_right[player_anim_count],
+                    (player.pos_x, player.pos_y))
         player_anim_count = (player_anim_count + 1) % len(walk_right)
     else:
-        screen.blit(walk_right[0], (player_x, player_y))
+        screen.blit(walk_right[0], (player.pos_x, player.pos_y))
 
     if player_anim_count == 2:
         player_anim_count = 0
