90782086d7853ed1dc48877a1bcd8b4c9144cb7e
Aisulu <aidarkyzyaisulu@gmail.com>
2024-04-17 17:14:46 +0500

    Fix animation and add images

--- game.py
+++ game.py
@@ -1,21 +1,24 @@
 import pygame
+
 clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("proj n11")
-icon = pygame.image.load('images/icon.png')
-pygame.display.set_icon(icon)
+# icon = pygame.image.load('images/icon.png')
+# pygame.display.set_icon(icon)
 
 myfont = pygame.font.SysFont('comicsansms', 40)
 text_surface = myfont.render('some text', True, 'Red')
 
 background = pygame.image.load('images/background.png')
+background = pygame.transform.scale(background, (800, 600))
+
 # player = pygame.image.load('images/player.png')
-walk_left = [
-    pygame.image.load('images/player_left/player_left1.png'),
-    pygame.image.load('images/player_left/player_left2.png'),
-    pygame.image.load('images/player_left/player_left3.png')
-]
+# walk_left = [
+#     pygame.image.load('images/player_left/player_left1.png'),
+#     pygame.image.load('images/player_left/player_left2.png'),
+#     pygame.image.load('images/player_left/player_left3.png')
+# ]
 walk_right = [
     pygame.image.load('images/player_right/player_right1.png'),
     pygame.image.load('images/player_right/player_right2.png'),
@@ -26,8 +29,9 @@ player_anim_count = 0
 running = True
 while running:
     screen.blit(background, (0, 0))
-    screen.blit(walk_right[player_anim_count], (300, 300))
-    if player_anim_count == 3:
+    screen.blit(walk_right[player_anim_count], (300, 420))
+
+    if player_anim_count == 2:
         player_anim_count = 0
     else:
         player_anim_count += 1
@@ -35,6 +39,7 @@ while running:
     # screen.blit(player, (200, 100))
 
     pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -43,4 +48,4 @@ while running:
             if event.key == pygame.K_a:
                 screen.fill((70, 44, 133))
 
-    clock.tick(60)
+    clock.tick(15)
